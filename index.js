1333e507ce858f24645f8c07342c82caad2868b4
CatThingy <andyc.andychen@gmail.com>
2020-06-29 00:39:11 -0700

    Implemented command to see events up to a month in the future

--- index.js
+++ index.js
@@ -6,6 +6,76 @@ const fetch = require("node-fetch");
 
 const config = require("./config.json");
 
+// Contains deabbrevations for US states / Canadian provinces
+const deabbreviation_dict = {
+    "ab": "Alberta",
+    "ak": "Alaska",
+    "al": "Alabama",
+    "ar": "Arkansas",
+    "az": "Arizona",
+    "bc": "British Columbia",
+    "ca": "California",
+    "co": "Colorado",
+    "ct": "Connecticut",
+    "dc": "District of Columbia",
+    "de": "Delaware",
+    "fl": "Florida",
+    "ga": "Georgia",
+    "hi": "Hawaii",
+    "ia": "Iowa",
+    "id": "Idaho",
+    "il": "Illinois",
+    "in": "Indiana",
+    "ks": "Kansas",
+    "ky": "Kentucky",
+    "la": "Louisiana",
+    "ma": "Massachusetts",
+    "mb": "Manitoba",
+    "md": "Maryland",
+    "me": "Maine",
+    "mi": "Michigan",
+    "mn": "Minnesota",
+    "mo": "Missouri",
+    "ms": "Mississippi",
+    "mt": "Montana",
+    "nb": "New Brunswick",
+    "nc": "North Carolina",
+    "nd": "North Dakota",
+    "ne": "Nebraska",
+    "nf": "Newfoundland",
+    "nh": "New Hampshire",
+    "nj": "New Jersey",
+    "nm": "New Mexico",
+    "ns": "Nova Scotia",
+    "nt": "Northwest Territory",
+    "nu": "Nunavut",
+    "nv": "Nevada",
+    "ny": "New York",
+    "oh": "Ohio",
+    "ok": "Oklahoma",
+    "on": "Ontario",
+    "or": "Oregon",
+    "pa": "Pennsylvania",
+    "pe": "Prince Edward Island",
+    "pei": "Prince Edward Island",
+    "qc": "Quebec",
+    "ri": "Rhode Island",
+    "sc": "South Carolina",
+    "sd": "South Dakota",
+    "sk": "Saskatchewan",
+    "tn": "Tennessee",
+    "tx": "Texas",
+    "ut": "Utah",
+    "va": "Virginia",
+    "vt": "Vermont",
+    "wa": "Washington",
+    "wi": "Wisconsin",
+    "wv": "West Virginia",
+    "wy": "Wyoming",
+    "yt": "Yukon"
+}
+const regions = Object.values(deabbreviation_dict).join(",").toLowerCase();
+
 const client = new Discord.Client();
 const settings = new Enmap({ name: "settings", fetchAll: false, autoFetch: true });
 
@@ -36,13 +106,41 @@ client.on("message", message => {
         if (command == "event") {
             fetch("https://api.vexdb.io/v1/get_events?sku=" + args[0])
                 .then(res => res.json())
-                .then(data => message.channel.send({ embed: formatEvent(data.result[0]) }))
+                .then(data => formatEvent(data.result[0]))
+                .then(embed => message.channel.send({ embed: embed }))
                 .catch(e => message.channel.send(`No event found with the SKU ${args[0]}.`));
+        }
+
+        // Get upcoming events in the region
+        if (command == "upcoming") {
+            let region = serverSettings.default_region;
+            if (args[0]) {
+                if (args[0].length <= 3) {
+                    region = deabbreviation_dict[args[0]];
+                }
+                else {
+                    region = args[0];
+                }
+            }
+            let url = "https://api.vexdb.io/v1/get_events?status=current,future";
+            if (regions.includes(region.toLowerCase())) {
+                url += "&region=" + region;
+            }
+            else {
+                url += "&country=" + region;
+            }
+            console.log(url);
+            fetch(url)
+                .then(res => res.json())
+                .then(data => findUpcoming(data.result, region))
+                .then(output => message.channel.send({ embed: output }))
+                .catch(e => {message.channel.send(`No upcoming events could be found in ${toTitleCase(region)}`)});
+
         }
     }
 });
 
-function formatEvent(eventData) {
+async function formatEvent(eventData) {
     let embed = {
         title: eventData.name,
         description: eventData.season,
@@ -103,4 +201,35 @@ function formatEvent(eventData) {
         value: new Date(eventData.start).toUTCString().substring(0, 16)
     })
     return embed;
+}
+
+async function findUpcoming(events, region) {
+    // Look 1 month in advance
+    let latestDate = new Date().setUTCHours(0,0,0,0) + 2592e6
+    let upcoming = [];
+    for (const event of events) {
+        if(Date.parse(event.end) < latestDate) {
+            upcoming.push(event);
+        }
+    }
+
+    let embed = {
+        title: "Upcoming events in " + toTitleCase(region),
+        description: "Future events up to " + new Date(latestDate).toUTCString().substring(0, 16),
+        fields: []
+    }
+
+    console.log(events);
+
+    for (const event of upcoming) {
+        embed.fields.push({
+            name: event.name,
+            value: `SKU: ${event.sku}\nDate: ${new Date(event.start).toUTCString().substring(5, 16)}`
+        });
+    }
+    return embed;
+}
+
+function toTitleCase(str) {
+    return str.replace(/\b(\w)(?!(\w{0,2}\b))/g, m => m.toUpperCase());
 }
