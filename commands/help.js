0502f0e7972f32fb5db95c4feda42f4094313844
CatThingy <andyc.andychen@gmail.com>
2020-06-30 13:38:43 -0700

    Improved help message

--- commands/help.js
+++ commands/help.js
@@ -2,22 +2,35 @@ module.exports = {
     name: "help",
     description: "Lists command syntax",
     execute(message, args, serverSettings) {
-        const helpMessage = 
-        "```css\nhelp\n" + (message.member.hasPermission("MANAGE_GUILD") ?
-        "config [setting|reset] [value?]\n" : "") +
-        "event [SKU]\n" +
-        "upcoming [region?] [timespan?]\n"+
-        "recent [region?] [timespan?]\n```";
-        message.channel.send({embed: {
-            title: "vex-info Command List",
-            url: "https://github.com/CatThingy/vex-info#commands",
-            fields: [
-                {
-                    name: "\u200b",
-                    value: helpMessage,
+        message.channel.send({
+            embed: {
+                author: {
+                    name: "Prefix: " + serverSettings.prefix
+                },
+                title: "vex-info Command List",
+                url: "https://github.com/CatThingy/vex-info#commands",
+                fields: [
+                    {
+                        name: "config [setting|reset]",
+                        value: "Gets or sets the configuration of the bot. Can only be used by members who have permission to manage the server.",
+                    },
+                    {
+                        name: "event [SKU]",
+                        value: "Displays information about the event with the specified SKU.",
+                    },
+                    {
+                        name: "upcoming [region?] [timespan?]",
+                        value: "Displays basic info about future events in the specified region, looking forward by the value of `timespan`.",
+                    },
+                    {
+                        name: "recent [region?] [timespan?]",
+                        value: "Displays basic info about past events in the specified region, looking back by the value of `timespan`.",
+                    }
+                ],
+                footer: {
+                    text: "Click top link for more detailed information."
                 }
-            ],
-            footer: "Click the top link for more detailed information."
-        }});
+            }
+        });
     }
 }
